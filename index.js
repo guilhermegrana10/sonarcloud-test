0fd1ca77222f9210a3a0c725bf62e8c40bdb6935
guilhermegrana10 <guilherme.grana@sp.senai.br>
2025-04-02 13:43:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,32 @@ function soma(a, b) {
   return a + b;
 }
 
-module.exports = soma;
+// Função duplicada (code smell)
+function somaDuplicada(x, y) {
+  return x + y;
+}
+
+// Função sem uso (code smell)
+function inutil() {
+  console.log('Nunca sou chamada');
+}
+
+// Uso de var (code smell e má prática)
+var resultado = soma(2, 3);
+
+// Comparação perigosa (bug potencial)
+if (resultado = 5) {  // aqui usamos "=" ao invés de "==="
+  console.log('Resultado é 5');
+}
+
+// Vulnerabilidade: função que executa código via eval
+function executaCodigo(codigo) {
+  eval(codigo);  // vulnerabilidade grave
+}
+
+module.exports = {
+  soma,
+  somaDuplicada,
+  inutil,
+  executaCodigo
+};
